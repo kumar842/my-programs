24821bea44ede1018ef28d16586b2654ec2cd9a7
Rajkumar Regunta <rregunta@proterra.com>
2020-05-24 00:08:12 +0530

    Revert "..."
    This reverts commit bacefd7a070fa565c59ba64dc762ff7283758c41.

--- javascript/web-components/app.js
+++ javascript/web-components/app.js
@@ -4,14 +4,9 @@ template.innerHTML = `
         h3 {
             color: coral;
         }
-        .image {
-            float: left;
-        }
     </style>
     <div class="user-card">
-        <div class="image">
-            <img />
-        </div>
+        <img />
         <div>
             <h3></h3>
             <div class="info">
@@ -57,87 +52,4 @@ export default class UserCard extends HTMLElement {
         this.shadowRoot.querySelector('#toggle-info').removeEventListener();
     }
 }
-customElements.define('user-card', UserCard);
-
-// let deferredPrompt;
-
-// window.addEventListener('beforeinstallprompt', (e) => {
-//   // Prevent the mini-infobar from appearing on mobile
-//   e.preventDefault();
-//   // Stash the event so it can be triggered later.
-//   deferredPrompt = e;
-//   // Update UI notify the user they can install the PWA
-//   showInstallPromotion();
-// });
-
-
-//make sure service workers are supported
-if('serviceWorker'  in navigator){
-    window.addEventListener('load', () => {
-        navigator.serviceWorker
-            .register('./sw-cached-site.js')
-            .then(reg => console.log(`service worker registered: ${reg}`))
-            .catch(error => console.log(`service worked error: ${error}`));
-    });
-}
-
-const divInstall = document.getElementById('installContainer');
-const butInstall = document.getElementById('butInstall');
-
-// Add a beforeinstallprompt event handler to the window object.
-// Save the event as a global variable; we'll need it later to show the prompt.
-// Unhide the install button.
-window.addEventListener('beforeinstallprompt', (event) => {
-    console.log('👍', 'beforeinstallprompt', event);
-    // Stash the event so it can be triggered later.
-    window.deferredPrompt = event;
-    // Remove the 'hidden' class from the install button container
-    divInstall.classList.toggle('hidden', false);
-  });
-
-
-//   Add a click event handler for the install button.
-//   Call prompt() on the saved beforeinstallprompt event.
-//   Log the results of the prompt.
-//   Set the saved beforeinstallprompt event to null.
-//   Hide the install button.
-  butInstall.addEventListener('click', () => {
-    console.log('👍', 'butInstall-clicked');
-    const promptEvent = window.deferredPrompt;
-    if (!promptEvent) {
-      // The deferred prompt isn't available.
-      return;
-    }
-    // Show the install prompt.
-    promptEvent.prompt();
-    // Log the result
-    promptEvent.userChoice.then((result) => {
-      console.log('👍', 'userChoice', result);
-      // Reset the deferred prompt variable, since
-      // prompt() can only be called once.
-      window.deferredPrompt = null;
-      // Hide the install button.
-      divInstall.classList.toggle('hidden', true);
-    });
-  });
-
-
-//   Add an appinstalled event handler to the window object.
-//   Log the install event to analytics or other mechanism.
-window.addEventListener('appinstalled', (event) => {
-    console.log('👍', 'appinstalled', event);
-    divInstall.classList.toggle('hidden', true);
-  });
-
-//Track how the PWA was launched #
-window.addEventListener('load', () => {
-if (navigator.standalone) {
-    console.log('Launched: Installed (iOS)');
-    divInstall.classList.toggle('hidden', true);
-} else if (matchMedia('(display-mode: standalone)').matches) {
-    console.log('Launched: Installed', divInstall);
-    divInstall.classList.toggle('hidden', true);
-} else {
-    console.log('Launched: Browser Tab');
-}
-});
+customElements.define('user-card', UserCard); 
